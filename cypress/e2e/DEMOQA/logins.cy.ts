4429ff844ee56be9a18e7d42ed161653fb28de38
atwozy <cristalnievera30@gmail.com>
2024-04-11 10:10:38 +0800

    latest

--- cypress/e2e/DEMOQA/logins.cy.ts
+++ cypress/e2e/DEMOQA/logins.cy.ts
@@ -172,7 +172,7 @@ describe('Tests All Elements', () => {
                     elementsPage.buttons.getDynamicButtonMessage().should('be.visible')
                 
                 })
-            })
+            }) 
         })
     })
 })
